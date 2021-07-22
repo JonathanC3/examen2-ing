8e66321d106b1ced42e0a5da262bbb0c8c1af644
JonathanC3 <jonathanc309@gmail.com>
2021-07-21 19:08:38 -0600

    prueba

---
+++ src/test/java/cr/ac/ucr/examen/demo/Examen2IngApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.examen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2IngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
