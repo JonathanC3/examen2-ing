07ba7a891ae6289e60db52c6afaf3039bc7ae858
JonathanC3 <jonathanc309@gmail.com>
2021-07-21 19:23:59 -0600

    Ejercicio-Jonathan Castro

---
+++ src/main/java/cr/ac/ucr/examen/demo/Ejercicio.java
@@ -0,0 +1,23 @@
+package cr.ac.ucr.examen.demo;
+
+public class Ejercicio {
+    private int id;
+
+    private String nombre;
+
+    public Ejercicio(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+}
+
+
